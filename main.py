aa1db4d12b3d7a0817ce765c9e1349fd93dd5178
UD-sama <141714552+UD-sama@users.noreply.github.com>
2023-08-10 13:52:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ai import generate_text
 from trans_v_to_t import speech_to_text
-from t_to_v import text_to_speech
-from yating import aitts
+from vits_generator import generate_audio
+from IPython.display import Audio
 
 
 
@@ -14,7 +14,22 @@ while(True):
     generated_text = generate_text(prompt_text)
     #輸出
     print(generated_text)
-    aitts(generated_text)
-    #tts_fn(model, hps, speaker_ids)
+    generate_audio(output_path="/content/VITS-fast-fine-tuning",
+               model_path="/content/drive/MyDrive/继续训练Anny300/G_latest.pth",
+               config_path="/content/drive/MyDrive/继续训练Anny300/finetune_speaker.json",
+               language="English",
+               text=generated_text,
+               spkid=0,
+               noise_scale=0.667,
+               noise_scale_w=0.6,
+               length_scale=1,
+               output_name="output")
+    output_audio_path = "/content/VITS-fast-fine-tuning/output.wav"  # 将路径替换为实际的音频文件路径
+    Audio(output_audio_path)
+    
+    
+    
+
+
 
 
